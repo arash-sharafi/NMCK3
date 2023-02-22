878f0f41108012ceb7934d1382417b11c6291597
Administrator <arash.sharafi.art@gmail.com>
2023-02-22 09:51:28 -0500

    Create CustomException class

---
+++ NMCK3.Shared/Exceptions/Mock3Exception.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NMCK3.Shared.Exceptions
+{
+    public abstract class Mock3Exception : Exception
+    {
+        protected Mock3Exception(string message) : base(message)
+        {
+
+        }
+    }
+}
