59219c3a6f99c24fcb6366196c3eae828f31c7a9
Administrator <arash.sharafi.art@gmail.com>
2023-02-22 20:31:13 -0500

    Add behavior to Participant Entity

--- NMCK3.Domain/Entities/Participant.cs
+++ NMCK3.Domain/Entities/Participant.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using NMCK3.Domain.Exceptions;
+using System;
 
 namespace NMCK3.Domain.Entities
 {
     public class Participant
     {
-        public Guid Id { get; set; }
-        public string Email { get; set; }
+        private Participant(Guid id, string email)
+        {
+            Id = id;
+            Email = email;
+        }
+
+        public Guid Id { get; private set; }
+        public string Email { get; private set; }
+
+        public static Participant Create(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                throw new NullParticipantEmailException();
+
+            var participant = new Participant(Guid.NewGuid(), email);
+
+            return participant;
+        }
     }
 }
---
+++ NMCK3.Domain/Exceptions/NullParticipantEmailException.cs
@@ -0,0 +1,11 @@
+﻿using NMCK3.Shared.Exceptions;
+
+namespace NMCK3.Domain.Exceptions
+{
+    public class NullParticipantEmailException:Mock3Exception
+    {
+        public NullParticipantEmailException() : base("Participant Email cannot be empty.")
+        {
+        }
+    }
+}
