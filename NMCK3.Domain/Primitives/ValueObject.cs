535a32bbf552f2e6af24d38aa21ad4da20fa3997
Administrator <arash.sharafi.art@gmail.com>
2023-02-23 19:58:08 -0500

    Introduce ValueObject type to the domain

---
+++ NMCK3.Domain/Primitives/ValueObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NMCK3.Domain.Primitives
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityComponents();
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(ValueObject other)
+        {
+            return Equals((object)other);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var valueObject = (ValueObject)obj;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Aggregate(
+                    default(int),
+                    HashCode.Combine);
+        }
+    }
+}
