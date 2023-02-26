38abb9f8b00210589b858469b12e66bdb3584ec2
Administrator <arash.sharafi.art@gmail.com>
2023-02-25 20:19:26 -0500

    Add CreateExam Command and Handler to the Application

---
+++ NMCK3.Application/Exams/Commands/Create/CreateExamCommand.cs
@@ -0,0 +1,10 @@
+﻿using NMCK3.Application.Abstractions.Messaging;
+
+namespace NMCK3.Application.Exams.Commands.Create
+{
+    public sealed record CreateExamCommand(
+        string Name,
+        string ExamDate,
+        string Description,
+        int Capacity) : ICommand;
+}
---
+++ NMCK3.Application/Exams/Commands/Create/CreateExamCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using NMCK3.Application.Abstractions.Messaging;
+using NMCK3.Application.Repositories;
+using NMCK3.Domain.Common;
+using NMCK3.Domain.Entities;
+using NMCK3.Domain.ValueObjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NMCK3.Application.Exams.Commands.Create
+{
+    internal sealed class CreateExamCommandHandler : ICommandHandler<CreateExamCommand>
+    {
+        private readonly IExamRepository _examRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateExamCommandHandler(IExamRepository examRepository, IUnitOfWork unitOfWork)
+        {
+            _examRepository = examRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(CreateExamCommand request, CancellationToken cancellationToken)
+        {
+            var examDate = ExamDate.Create(request.ExamDate);
+            var result = Exam.Create(request.Name, examDate.Value, request.Description, request.Capacity);
+
+
+            if (result.IsFailure)
+            {
+                return Result.Fail(result.Error);
+            }
+
+            _examRepository.Add(result.Value);
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success();
+        }
+    }
+}
