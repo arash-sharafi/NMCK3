9bd9f4f0ca630b2e21fc2da4045ef31a3da49d6a
Administrator <arash.sharafi.art@gmail.com>
2023-02-25 20:02:52 -0500

    Add CQRS with Mediator in the Application

---
+++ NMCK3.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using NMCK3.Domain.Common;
+
+namespace NMCK3.Application.Abstractions.Messaging
+{
+    public interface ICommand : IRequest<Result>
+    {
+    }
+
+    public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+    {
+    }
+}
---
+++ NMCK3.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NMCK3.Domain.Common;
+
+namespace NMCK3.Application.Abstractions.Messaging
+{
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
+        where TCommand : ICommand
+    {
+    }
+
+    public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+        where TCommand : ICommand<TResponse>
+    {
+    }
+}
